491c4ecc2e5195440bc610beac23514317cf42a0
Catalin Epingeac <ionutepingeac@gmail.com>
2020-08-05 18:22:24 +0300

    Add .NET Core Project

---
+++ FBARefundProject/FBARefundProject/Controllers/ReportsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FBARefundProject.Models.Reports;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FBARefundProject.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public ActionResult Index(int SellerId)
+        {
+            ReportsViewModel model = new ReportsViewModel();
+            //TODO Get from DB
+            model.SellerId = "ABS2322A";
+            model.ReportsGridItems.Add(new ReportsGridItem()
+            {
+                Actual = "",
+                AmazonStatus = AmazonStatuses.Open,
+                CaseId = 213123132,
+                CaseStatus = CaseStatuses.Success,
+                Details = "Units damaged in warehouse were never reimbursed",
+                Price = 400,
+                Quantity = 2,
+                FilledOn = DateTime.Now
+            });
+
+            return View(model);
+        }
+
+        public ActionResult AmazonApi()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ FBARefundProject/FBARefundProject/Models/Reports/ReportsGridItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FBARefundProject.Models.Reports
+{
+    public class ReportsGridItem
+    {
+        public DateTime FilledOn { get; set; }
+        public AmazonStatuses AmazonStatus { get; set; }
+        public CaseStatuses CaseStatus { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Actual { get; set; }
+        public long CaseId { get; set; } //maybe guid?
+        public string Details { get; set; }
+        public string MoreDetails { get; set; } // is needed?
+    }
+
+
+    public enum AmazonStatuses { 
+        Open,
+        Pending,
+        Closed
+    }
+
+    public enum CaseStatuses
+    {
+        Pending,
+        Success,
+        Failed,
+        Closed
+    }
+}
